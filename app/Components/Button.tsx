f01b90a7826b60521417a8a40d2eef499a99cb77
Ashish <ashish74624@gmail.com>
2023-10-26 17:08:45 +0530

    Hero section completed

---
+++ app/Components/Button.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+import { motion } from "framer-motion"
+
+
+export default function Button() {
+  return (
+    <motion.button whileHover={{scale:1.1}} whileTap={{scale:0.5}} className=' px-4 py-2 rounded-lg text-lg text-white mx-auto border-white border mt-4 hover:bg-[#E31B6D] transition-all duration-200'>
+        View my Work
+    </motion.button>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Fugaz_One } from "next/font/google"
 import { Noto_Sans_Javanese } from "next/font/google"
+import Button from "./Components/Button"
 
 
 const Nsj = Noto_Sans_Javanese({
@@ -16,8 +17,9 @@ export default function Home() {
 
   return (
     <main className='bg-[#0F172A] h-screen w-screen overflow-x-hidden overflow-y-scroll relative flex justify-center items-center'>
-        <div className='bg-[#6a8bd7] absolute  w-80 h-80 rounded-full blur-lg bottom-[75vh] right-[85vw] animate-pulse'></div>
-        <div className='bg-[#6a8bd7] absolute  w-80 h-80 rounded-full blur-lg bottom-[75vh] right-[85vw] animate-pulse'></div>
+        <div className='bg-[#6a8bd7] absolute  w-80 h-80 rounded-full blur-lg bottom-[75vh] right-[85vw] animate-pulse hover:w-96 hover:h-96 transition-all'></div>
+        <div className={` w-[2px] h-[500px] bg-white absolute top-0 right-0 rotate-45 origin-top `}></div>
+        <div className=" w-[2px] h-[500px] bg-white absolute top-[50vh] rotate-45 left-[351px] origin-top"></div>
         <div className='bg-[#E31B6D] absolute  w-20 h-20 rounded-full blur-md animate-bounce bottom-1 right-4'></div>
         <div className='z-10 flex flex-col justify-center w-max h-max border-white border rounded-lg px-10 py-8 relative '>
             <span className=' w-4 h-4 rounded-full bg-white absolute bottom-[194px] left-[609px] animate-ping'></span>
@@ -25,9 +27,7 @@ export default function Home() {
                 Hello There, <span className='text-[#E31B6D]'>I'm Ashish Kumar</span>. <br />
                 I am a full stack web Developer.
             </p>
-            <button className=' px-4 py-2 rounded-lg text-lg text-white mx-auto border-white border mt-4 hover:bg-[#E31B6D] transition-all duration-200'>
-                View my Work
-            </button>
+            <Button/>
         </div>  
     </main>
   )
