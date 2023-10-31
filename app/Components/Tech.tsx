9f4f09a1057c4777d04646652fde1c6a11304f93
Ashish <ashish74624@gmail.com>
2023-11-01 01:45:19 +0530

    Website made responsive

--- app/Components/Tech.tsx
+++ app/Components/Tech.tsx
@@ -43,10 +43,10 @@ export default function Tech() {
   return (
     <section className=' w-screen h-max bg-white overflow-hidden flex flex-col items-center '>
       <h2 className=' my-8 text-4xl text-gray-800 font-semibold underline-offset-8 underline'>Tech Stack</h2>
-      <div className=' w-[900px] mx-auto grid grid-cols-3 gap-4 mb-8'>
+      <div className=' w-screen overflow-y-hidden overflow-x-scroll md:w-max mx-auto flex md:grid md:grid-cols-3 gap-4 mb-8 md:p-8 px-2'>
         {tech.map((item)=>(
           <Tilt  options={defaultOptions} >
-        <div key={item.id} className=' bg-teal-600 text-white text-xl rounded-lg h-64 w-48 flex flex-col justify-center items-center '>
+        <div key={item.id} className=' bg-teal-600 text-white text-xl rounded-lg w-36 h-56 md:h-64 md:w-48 flex flex-col justify-center items-center '>
             {item.icon}
             {item.title}
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
         <div className={` w-[2px] h-[500px] bg-white absolute top-0 right-0 rotate-45 origin-top `}></div>
         <div className=" w-[2px] h-[500px] bg-white absolute top-[50vh] rotate-45 left-[351px] origin-top"></div>
         <div className='bg-[#E31B6D] absolute  w-20 h-20 rounded-full blur-md animate-bounce bottom-1 right-4'></div>
-        <div className='z-10 flex flex-col justify-center w-[360px] lg:w-[610px] lg:bg-none bg-[#0F172A] h-max border-white border rounded-lg  py-8 relative '>
-            <span className=' w-4 h-4 rounded-full bg-white absolute bottom-[194px] left-[601px] animate-ping'></span>
-            <p className='text-white text-2xl lg:text-4xl w-max mx-auto'>
+        <div className='z-10 flex flex-col justify-center w-[360px] md:w-[610px] lg:bg-none bg-[#0F172A] h-52 border-white border rounded-lg  py-8 relative '>
+            <span className=' w-4 h-4 rounded-full bg-white absolute bottom-[200px] left-[351px] md:left-[601px] animate-ping'></span>
+            <p className='text-white text-2xl md:text-4xl w-max mx-auto'>
                 Hello There, <span className='text-[#E31B6D]'>I'm Ashish Kumar</span>. <br />
                 I am a full stack web Developer.
             </p>
