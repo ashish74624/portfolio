d9d796aa06c7ad92d3317141a8cb5a11b7b52538
Ashish <ashish74624@gmail.com>
2023-11-01 16:17:59 +0530

    Changed my intro

--- app/Components/About.tsx
+++ app/Components/About.tsx
@@ -13,7 +13,7 @@ export default function About() {
       <div className=' flex md:flex-row flex-col mx-0 md:mx-auto md:w-max'>
         <Image className='h-72 w-72 lg:h-72 lg:w-72 md:h-60 md:w-60 rounded-full md:mx-0 mx-auto' src={"/me.png"} alt='Me' height={160} width={160}/>
         <p className='text-xl md:text-2xl m-6'>
-          Hello there! I&#39;m Ashish Kumar. <br /> I&#39;m full-stack developer from Delhi, India. <br /> Currently pursuing my BTech in Bangalore, <br /> I&#39;m enthusiastic about crafting seamless web solutions.
+          I&#39;m Ashish Kumar. <br /> I&#39;m full-stack developer from Delhi, India. <br /> Currently pursuing my BTech in Bangalore, <br /> I&#39;m enthusiastic about crafting seamless web solutions.
           <p className=' text-base flex w-80 justify-between items-center h-10 font-sans mt-4'>
           <Mail/> ashish74624@gmail.com | <a href="https://www.linkedin.com/in/ashish-kumar-05386722a" target="_blank" rel="noopener noreferrer"><Linkedin/></a> | <a href="https://github.com/ashish74624" target="_blank" rel="noopener noreferrer"><Github/></a> 
           </p> 
