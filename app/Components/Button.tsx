5b911e08f4869159636c8ddca273bc548d891b54
Ashish <ashish74624@gmail.com>
2023-11-02 01:00:26 +0530

    xs breakpoint added

--- app/Components/Button.tsx
+++ app/Components/Button.tsx
@@ -5,8 +5,8 @@ import { motion } from "framer-motion"
 
 export default function Button() {
   return (
-    <a className='inline h-12 w-36 rounded-lg mx-auto' href="#here">
-      <motion.button whileHover={{scale:1.1}} whileTap={{scale:0.85}} className=' h-12 w-36 rounded-lg text-lg text-white  border-white border mt-4 hover:bg-[#E31B6D] transition-all duration-200'>
+    <a className='inline h-10 w-28 xs:h-12 xs:w-36 rounded-lg mx-auto' href="#here">
+      <motion.button whileHover={{scale:1.1}} whileTap={{scale:0.85}} className=' text-base h-10 w-28 xs:h-12 xs:w-36 rounded-lg xs:text-lg text-white  border-white border mt-4 hover:bg-[#E31B6D] transition-all duration-200'>
         View more
       </motion.button>
     </a>
