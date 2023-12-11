c2ba6de35e30565639249ffd02e25eff5658156d
Ashish <ashish74624@gmail.com>
2023-12-11 18:21:20 +0530

    changed scroll id to #me

--- app/Components/Button.tsx
+++ app/Components/Button.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion"
 
 export default function Button() {
   return (
-    <a className='inline h-10 w-28 xs:h-12 xs:w-36 rounded-lg mx-auto' href="#here">
+    <a className='inline h-10 w-28 xs:h-12 xs:w-36 rounded-lg mx-auto' href="#me">
       <motion.button whileHover={{scale:1.1}} whileTap={{scale:0.85}} className=' text-base h-10 w-28 xs:h-12 xs:w-36 rounded-lg xs:text-lg text-white  border-white border mt-4 hover:bg-[#E31B6D] transition-all duration-200'>
         View more
       </motion.button>
